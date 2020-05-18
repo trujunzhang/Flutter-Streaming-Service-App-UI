0d5196ee620ffedd763e89f951e35929a54c42ce
djzhang <trujunzhang@gmail.com>
2020-05-18 18:42:45 +0800

    Flutter-Streaming-Service-App-UI

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/djzhang/Documents/flutterSDK"
+export "FLUTTER_APPLICATION_PATH=/Users/djzhang/Documents/Organizations/__flutter/tutors/_devefy/Flutter-Streaming-Service-App-UI"
+export "FLUTTER_TARGET=/Users/djzhang/Documents/Organizations/__flutter/tutors/_devefy/Flutter-Streaming-Service-App-UI/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/djzhang/Documents/flutterSDK/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
