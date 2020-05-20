f7347c235f0f01ba7105798f413189486ce531a0
djzhang <trujunzhang@gmail.com>
2020-05-20 09:37:36 +0800

    PhotoView

--- lib/src/restaurant_detail/widget/top_part.dart
+++ lib/src/restaurant_detail/widget/top_part.dart
@@ -10,125 +10,121 @@ class ScreenTopPart extends StatefulWidget {
 }
 
 class _ScreenTopPartState extends State<ScreenTopPart> {
-  @override
-  Widget build(BuildContext context) {
+  Widget buildBg() {
     var image = Image.asset("assets/images/banner.png",
         fit: BoxFit.cover, width: double.infinity);
+    return ClipPath(
+      clipper: Mclipper(),
+      child: Container(
+        height: 370.0,
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0.0, 10.0),
+              blurRadius: 10.0)
+        ]),
+        child: Stack(
+          children: <Widget>[
+            image,
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                    const Color(0x00000000),
+                    const Color(0xD9333333)
+                  ],
+                      stops: [
+                    0.0,
+                    0.9
+                  ],
+                      begin: FractionalOffset(0.0, 0.0),
+                      end: FractionalOffset(0.0, 1.0))),
+              child: Padding(
+                padding: EdgeInsets.only(top: 120.0, left: 95.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "The Punisher: Season 2",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 45.0,
+                          fontFamily: "SF-Pro-Display-Bold"),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildFg() {
+    return Positioned(
+      top: 370.0,
+      right: -20.0,
+      child: FractionalTranslation(
+        translation: Offset(0.0, -0.5),
+        child: Row(
+          children: <Widget>[
+            FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () {},
+              child: Icon(
+                Icons.edit,
+                color: Color(0xFFE52020),
+              ),
+            ),
+            SizedBox(
+              width: 8.0,
+            ),
+            FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () {},
+              child: Icon(
+                Icons.photo_camera,
+                color: Color(0xFFE52020),
+              ),
+            ),
+            SizedBox(
+              width: 8.0,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(30.0),
+              child: RaisedButton(
+                onPressed: () {},
+                color: Color(0xFFE52020),
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 80.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Write a review",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                          fontFamily: "SF-Pro-Display-Bold"),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return new Container(
       height: 420.0,
       child: Stack(
-        children: <Widget>[
-          ClipPath(
-            clipper: Mclipper(),
-            child: Container(
-              height: 370.0,
-              decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                BoxShadow(
-                    color: Colors.black12,
-                    offset: Offset(0.0, 10.0),
-                    blurRadius: 10.0)
-              ]),
-              child: Stack(
-                children: <Widget>[
-                  image,
-                  Container(
-                    height: double.infinity,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            colors: [
-                          const Color(0x00000000),
-                          const Color(0xD9333333)
-                        ],
-                            stops: [
-                          0.0,
-                          0.9
-                        ],
-                            begin: FractionalOffset(0.0, 0.0),
-                            end: FractionalOffset(0.0, 1.0))),
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 120.0, left: 95.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            "The Punisher: Season 2",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 45.0,
-                                fontFamily: "SF-Pro-Display-Bold"),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            top: 370.0,
-            right: -20.0,
-            child: FractionalTranslation(
-              translation: Offset(0.0, -0.5),
-              child: Row(
-                children: <Widget>[
-                  FloatingActionButton(
-                    backgroundColor: Colors.white,
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.edit,
-                      color: Color(0xFFE52020),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 8.0,
-                  ),
-                  FloatingActionButton(
-                    backgroundColor: Colors.white,
-                    onPressed: () {},
-                    child: Icon(
-                      Icons.photo_camera,
-                      color: Color(0xFFE52020),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 8.0,
-                  ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(30.0),
-                    child: RaisedButton(
-                      onPressed: () {},
-                      color: Color(0xFFE52020),
-                      padding: EdgeInsets.symmetric(
-                          vertical: 15.0, horizontal: 80.0),
-                      child: Row(
-                        children: <Widget>[
-//                          RotatedBox(
-//                            quarterTurns: 2,
-//                            child: Icon(Icons.rate_review,
-//                                size: 25.0, color: Colors.white),
-//                          ),
-//                          SizedBox(
-//                            width: 5.0,
-//                          ),
-                          Text(
-                            "Write a review",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15.0,
-                                fontFamily: "SF-Pro-Display-Bold"),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
+        children: <Widget>[buildBg(), buildFg()],
       ),
     );
   }
