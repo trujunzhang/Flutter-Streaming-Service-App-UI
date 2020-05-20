14b5d3433a67a6ee350551098f9b458245c59c5a
djzhang <trujunzhang@gmail.com>
2020-05-20 09:33:15 +0800

    PhotoView

--- lib/src/restaurant_detail/widget/bottom_part.dart
+++ lib/src/restaurant_detail/widget/bottom_part.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'photo_view.dart';
+
 class ScreenBottomPart extends StatefulWidget {
   ScreenBottomPart({Key key}) : super(key: key);
 
@@ -20,48 +22,7 @@ class _ScreenBottomPartState extends State<ScreenBottomPart> {
     List<Widget> movieList = new List();
 
     for (int i = 0; i < 3; i++) {
-      var movieitem = Padding(
-        padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 12.0),
-        child: Container(
-          height: 220.0,
-          width: 135.0,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 10.0,
-                    offset: Offset(0.0, 10.0))
-              ]),
-          child: Column(
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0)),
-                child: Image.asset(
-                  images[i],
-                  width: double.infinity,
-                  height: 130.0,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 4.0, left: 8.0, right: 8.0),
-                child: Text(titles[i],
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 16.0, fontFamily: "SF-Pro-Display-Bold")),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 3.0),
-                child: Text(i == 0 ? "Season 2" : ""),
-              )
-            ],
-          ),
-        ),
-      );
+      var movieitem = PhotoView();
       movieList.add(movieitem);
     }
     return movieList;
--- lib/src/restaurant_detail/widget/photo_view.dart
+++ lib/src/restaurant_detail/widget/photo_view.dart
@@ -10,8 +10,28 @@ class PhotoView extends StatefulWidget {
 class _PhotoViewState extends State<PhotoView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Container(),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 12.0),
+      child: Container(
+        height: 220.0,
+        width: 135.0,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10.0,
+                  offset: Offset(0.0, 10.0))
+            ]),
+        child:
+        Image.asset(
+          "assets/images/runaways.jpg",
+          width: double.infinity,
+          fit: BoxFit.cover,
+        ),
+
+      ),
     );
   }
 }
